a3755e6338b9eba06ed8e8cedbe21f5c29468596
zeyadsalah22 <112489529+zeyadsalah22@users.noreply.github.com>
2024-08-30 07:36:23 +0300

    as

--- Portfolio/static/portfolio/script.js
+++ Portfolio/static/portfolio/script.js
@@ -1,34 +1,36 @@
-document.addEventListener('DOMContentLoaded',function(){
+document.addEventListener('DOMContentLoaded', function() {
 
-    function getCookie(name) {
-        const value = `; ${document.cookie}`;
-        const parts = value.split(`; ${name}=`);
-        if (parts.length == 2) return parts.pop().split(';').shift();
-    }
+  function getCookie(name) {
+      const value = `; ${document.cookie}`;
+      const parts = value.split(`; ${name}=`);
+      if (parts.length === 2) return parts.pop().split(';').shift();
+  }
 
-    document.querySelector("#contact-form").onsubmit = () =>{
-        const name = document.querySelector('#name').value;
-        const email = document.querySelector('#email').value;
-        const message = document.querySelector('#message').value;
-         fetch("/contact",{
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-CSRFToken': getCookie('csrftoken')
-            },
-            body: JSON.stringify({
+  document.querySelector("#contact-form").onsubmit = () => {
+      const name = document.querySelector('#name').value;
+      const email = document.querySelector('#email').value;
+      const message = document.querySelector('#message').value;
+      
+      fetch("/contact", {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+              'X-CSRFToken': getCookie('csrftoken')
+          },
+          body: JSON.stringify({
               name: name,
               email: email,
               message: message
-            })
-         })
-         .then(response => response.json())
-         .then(result => {
-            document.querySelector('#name').value = '';
-            document.querySelector('#email').value = '';
-            document.querySelector('#message').value = '';
-            console.log(result);
-         })
-        return false;
-      }
-    });
+          })
+      })
+      .then(response => response.json())
+      .then(result => {
+          document.querySelector('#name').value = '';
+          document.querySelector('#email').value = '';
+          document.querySelector('#message').value = '';
+          console.log(result);
+      });
+
+      return false;
+  }
+});
